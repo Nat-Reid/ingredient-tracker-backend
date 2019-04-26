b07820a9dd170db0f3c95ca8663e0e394b649ce4
Nat-Reid <nathanielreid@gmail.com>
2019-04-26 10:15:10 -0700

    moved auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  skip_before_action :authorized, only: [:create]
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: { user: UserSerializer.new(@user) }, status: :created
+    else
+      render json: { error: @user.errors.full_messages}, status: :not_acceptable
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :name)
+  end
 end
