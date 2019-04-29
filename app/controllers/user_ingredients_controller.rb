2c37622d7b8f3928e28c3e8b8fe55ca96d5f053d
Nat-Reid <nathanielreid@gmail.com>
2019-04-29 11:00:56 -0700

    properly serialized user_ingredients

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -1,7 +1,7 @@
 class UserIngredientsController < ApplicationController
 
   def index
-    render json: @user.ingredients
+    render json: @user.user_ingredients, each_serializer: UserIngredientSerializer
   end
 
 
--- app/serializers/user_ingredient_serializer.rb
+++ app/serializers/user_ingredient_serializer.rb
@@ -1,4 +1,4 @@
 class UserIngredientSerializer < ActiveModel::Serializer
-  attributes :id, :expiration_date, :quantity
+  attributes :id, :expiration_date, :quantity, :name
   belongs_to :ingredient
 end
