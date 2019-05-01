5e6822ffaa7d8885d14f20c0afaa93ae23aee24d
Nat-Reid <nathanielreid@gmail.com>
2019-05-01 09:45:33 -0700

    blocked non alphabetic API querys

--- app/controllers/concerns/spoonacular_helper.rb
+++ app/controllers/concerns/spoonacular_helper.rb
@@ -1,10 +1,15 @@
 module Concerns::SpoonacularHelper
   def ingredient_request(ingredient_name)
-    response = Unirest.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?number=100&query=#{ingredient_name}",
-            headers:{
-              "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-              "X-RapidAPI-Key" => ENV["SPOONACULAR_KEY"]
-            }
-    response.body.map{|ingredient| Ingredient.create(name: ingredient["name"])}
+    if (ingredient_name.match(/^[[:alpha:][:blank:]]+$/))
+      byebug
+      response = Unirest.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?number=10&query=#{ingredient_name}",
+              headers:{
+                "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+                "X-RapidAPI-Key" => ENV["SPOONACULAR_KEY"]
+              }
+      {json: response.body.map{|ingredient| Ingredient.create(name: ingredient["name"])}}
+    else
+      {json: {message: "Only letters can be used"}, status: :bad_request}
+    end
   end
 end
--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -13,7 +13,7 @@ class IngredientsController < ApplicationController
     if !@ingredient
       @matching_ingredients = Ingredient.where("ingredients.name  LIKE ?", params[:ingredient_name].downcase)
       if @matching_ingredients.blank?
-        render json: ingredient_request(params[:ingredient_name]) #from SpoonacularHelper
+        render ingredient_request(params[:ingredient_name]) #from SpoonacularHelper
       else
         render json: @matching_ingredients, each_serialzer: IngredientSerializer
       end
