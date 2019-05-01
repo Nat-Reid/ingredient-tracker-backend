9331e5956bc40b1f3aee7d9e188ddcd833ad4c16
Nat-Reid <nathanielreid@gmail.com>
2019-05-01 10:27:49 -0700

    ingredients seed with relational ids

--- app/controllers/concerns/spoonacular_helper.rb
+++ app/controllers/concerns/spoonacular_helper.rb
@@ -1,15 +1,19 @@
 module Concerns::SpoonacularHelper
   def ingredient_request(ingredient_name)
-    if (ingredient_name.match(/^[[:alpha:][:blank:]]+$/))
+    if (ingredient_name.match(/^[a-z ]+$/))
       byebug
-      response = Unirest.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?number=10&query=#{ingredient_name}",
+      response = Unirest.get "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/food/ingredients/autocomplete?metaInformation=true&number=10&query=#{ingredient_name}",
               headers:{
                 "X-RapidAPI-Host" => "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
                 "X-RapidAPI-Key" => ENV["SPOONACULAR_KEY"]
               }
-      {json: response.body.map{|ingredient| Ingredient.create(name: ingredient["name"])}}
+      {json: response.body.map{|ingredient| create_ingredient(ingredient)}, each_serialzer: IngredientSerializer}
     else
       {json: {message: "Only letters can be used"}, status: :bad_request}
     end
+  end
+
+  def create_ingredient(ingredient_hash)
+    Ingredient.create(name: ingredient["name"], id: ingredient["id"])
   end
 end
