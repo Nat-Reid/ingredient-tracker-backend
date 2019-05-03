6c8777e933559d050d6d81734e8022ff1f2a8686
Nat-Reid <nathanielreid@gmail.com>
2019-05-03 10:17:25 -0700

    create user ingredients

--- app/controllers/user_ingredients_controller.rb
+++ app/controllers/user_ingredients_controller.rb
@@ -4,5 +4,20 @@ class UserIngredientsController < ApplicationController
     render json: @user.user_ingredients, each_serializer: UserIngredientSerializer
   end
 
+  def create
+    # byebug
+    @user_ingredient = @user.user_ingredients.build(user_ingredient_params)
+    if @user_ingredient.save
+      render json: {user_ingredient: UserIngredientSerializer.new(@user_ingredient)}, status: 201
+    else
+      render json: {message: @user_ingredient.errors.full_messages}, status: 422
+    end
+  end
+
+  private
+
+  def user_ingredient_params
+    params.require(:user_ingredient).permit(:ingredient_id,:expiration_date,:quantity)
+  end
 
 end
